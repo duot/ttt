381e8dc260860aa091f9684811575e324294c353
James <duot.jim@gmail.com>
2019-12-29 11:53:34 -0800

    Square#marker validation added

--- square.rb
+++ square.rb
@@ -2,11 +2,13 @@ require_relative 'errors.rb'
 
 class Square
   EMPTY = ' '
+  MARKER_LENGTH = 1
+  MARKERS_ALLOWED = /[[:graph:]]+/
 
   attr_reader :marker, :number
 
   def initialize(number, marker = nil)
-    @marker = marker
+    @marker = marker.nil? ? marker : validate(marker)
     @number = number
   end
 
@@ -17,6 +19,7 @@ class Square
   def empty?; !marker; end
 
   def marker=(marker)
+    validate(marker)
     raise SquareAlreadyMarkedError if !empty?
     @marker = marker
   end
@@ -26,4 +29,12 @@ class Square
   end
 
   def to_s; symbol; end
+
+  private
+
+  def validate(marker)
+    raise ArgumentError, 'Too long' if marker.size != MARKER_LENGTH
+    raise ArgumentError, 'Invalid marker' unless MARKERS_ALLOWED.match? marker
+    marker
+  end
 end
